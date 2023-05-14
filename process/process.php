a56d354c06070283bc2594461972e8893bd5b20c
kitt-null <130538125+kitt-null@users.noreply.github.com>
2023-05-14 20:44:26 +0800

    fix

--- process/process.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-    if($_POST['username'] == 'admin' && $_POST['password'] == 'admin'){
-        echo 'login successful';
-    }else{
-        echo 'login failed';
-    }
-?>
--- router/router.php
+++
@@ -1,7 +0,0 @@
-<?php
-    require_once('../class/class.accounts.php');
-    $accounts = new Accounts;
-    if($_GET['ref'] == 'login'){
-        $accounts->login($_POST['username'],$_POST['password']);
-    }
-?>
